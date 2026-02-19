7cf4ab1c9d652e6fc8fbb18d2b87669b3759cdec
ferrosalt3-cmd <ferrosalt3@gmail.com>
2026-02-19 13:46:25 -0500

    Octavo código de inventario

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,10 @@ config = {
 }
 
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    cookie_key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days']
 )
 
 name, authentication_status, username = authenticator.login(location="main")

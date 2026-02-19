c405c93d32a2c8c8875fb6ecc07f542d40b02f6e
ferrosalt3-cmd <ferrosalt3@gmail.com>
2026-02-19 13:44:27 -0500

    Octavo código de inventario

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login(location="main")
 
 if authentication_status is False:
     st.error("Usuario o contraseña incorrectos")
